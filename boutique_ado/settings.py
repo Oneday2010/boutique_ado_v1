83e4717f71f470c15916c80d3ec3bdc001b8a8b5
Oneday2010 <jozxz1691@gmail.com>
2021-12-24 20:53:13 +0000

    upload media

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'xlnxg!5r1z&-+ffkp$$1)obtty(wah^q87tznx&3xyjgfwg7jg')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
